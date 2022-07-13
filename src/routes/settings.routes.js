0e796e6f6c733cf73d91605cbf655e1f3060e829
Dennis Fink <dennis.fink@kernpunkt.de>
2022-07-13 11:31:50 +0200

    Edit invoice and invoice tax id

--- src/routes/settings.routes.js
+++ src/routes/settings.routes.js
@@ -5,7 +5,6 @@ const SettingsController = require('../controllers/settings.controller')
 
 router.get('/general-information', SettingsController.getGeneralInformation)
 router.post('/general-information', SettingsController.createGeneralInformation)
-
 router.patch('/general-information/:id', SettingsController.updateGeneralInformationById)
 
 module.exports = router
